5938f47830c544e41abe078a9182b2837ac7199e
Liam Arbuckle <liam@skinetics.tech>
2024-01-16 22:29:03 +1100

    🥦🩼 ↝ landlubbers can't touch my rubies

--- config/initializers copy 9/good_job.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.configure do
-  config.good_job.enable_cron = true
-  config.good_job.cron = {
-    maintenance: {
-      cron: "0 22 * * *",
-      class: "DailyExchangeRateJob"
-    }
-  }
-end
